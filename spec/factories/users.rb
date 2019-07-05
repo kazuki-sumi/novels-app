a24964fef3a4681d2d6c878f4b78bf9a06a84f80
鷲見和希 <cs5200@icloud.com>
2019-07-05 17:58:02 +0900

    factoryフォルダ作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  image           :string(255)
+#  name            :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+FactoryBot.define do
+  factory :user do
+    
+  end
+end
