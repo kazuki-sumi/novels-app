a907b2d7ad8fd86e520a3a2dd254470d0ff8c6e0
鷲見和希 <cs5200@icloud.com>
2019-07-05 18:57:48 +0900

    annotate反映

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  image           :string(255)
+#  name            :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
