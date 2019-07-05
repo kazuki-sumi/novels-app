5f17e7931f866c2d287c3f45a98a7ba2517bce3c
鷲見和希 <cs5200@icloud.com>
2019-07-05 18:04:01 +0900

    annotate反映

--- app/models/submit_novel.rb
+++ app/models/submit_novel.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: submit_novels
+#
+#  id         :integer          not null, primary key
+#  postscript :text(65535)
+#  preface    :text(65535)
+#  subtitle   :string(255)
+#  text       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  draft_id   :integer
+#
+# Indexes
+#
+#  index_submit_novels_on_draft_id  (draft_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (draft_id => drafts.id)
+#
+
 class SubmitNovel < ApplicationRecord
   belongs_to :draft
   has_one :novel, through: :draft
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string(255)
+#  image           :string(255)
+#  name            :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
