f569a4910787722c3500ecccb0c4f67709dc74f9
鷲見和希 <cs5200@icloud.com>
2019-08-25 20:42:37 +0900

    fix

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -23,12 +23,10 @@
 class Favorite < ApplicationRecord
   belongs_to :novel
   belongs_to :user
-  
+
   validates :user_id, presence: true
   validates :novel_id, presence: true
-  
-  
-  
+
   def self.ranking
     self.group(:novel_id).order('count_novel_id DESC').limit(10).count(:novel_id)
   end
