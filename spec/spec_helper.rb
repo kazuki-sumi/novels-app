fd88fa6678e1bf153acb274e60429c871862676c
鷲見和希 <cs5200@icloud.com>
2019-08-25 23:54:52 +0900

    simplecovの設定

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,11 +15,18 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require "capybara/rspec"
+require 'simplecov'
 
 RSpec.configure do |config|
+  ENV['RAILS_ENV'] = 'test'
   config.before(:each, type: :system) do
     driven_by :selenium_chrome_headless
   end
+  if ENV['CIRCLE_ARTIFACTS']
+    dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+    SimpleCov.coverage_dir(dir)
+  end
+  SimpleCov.start
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
